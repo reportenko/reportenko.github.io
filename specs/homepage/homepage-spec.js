31fa987a19493eb6c5f9fac560e5eaf84279526e
Sergii Khromchenko <khromchenko13@gmail.com>
2021-12-11 22:09:22 -0500

    run tests

--- specs/homepage/homepage-spec.js
+++ specs/homepage/homepage-spec.js
@@ -4,13 +4,13 @@ describe('Homepage', () => {
     it('should not be able to login with none matching credentials', () => {
         browser.url('./');
 
-        browser.debug();
+        // browser.debug();
 
         // Click on ModelS link on the top of the scree
-        Homepage.modelSLnk.waitForClickable();
-        Homepage.modelSLnk.click();
+        // Homepage.modelSLnk.waitForClickable();
+        // Homepage.modelSLnk.click();
 
         // Wait for order button to appear
-        $('.hero-region--center-bottom [data-gtm-interaction="order now"]').waitForClickable();
+        // $('.hero-region--center-bottom [data-gtm-interaction="order now"]').waitForClickable();
     })
 })
